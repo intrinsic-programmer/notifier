7bf1ed84dbf45409bf4fd52d64c3b8735ab2e6d3
Joy <gajjarjoy4@gmail.com>
2021-06-03 12:32:00 +0530

    Windows Notifier

---
+++ main.py
@@ -0,0 +1,5 @@
+from plyer import notification
+from time import sleep
+
+notification.notify(title='Hey Joy!!', message='Thankyou for reading! Takecare!', app_name='python', app_icon=None, timeout=10,  toast=False)
+sleep(1)
